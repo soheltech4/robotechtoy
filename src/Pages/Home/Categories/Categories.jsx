0dfc61131e74790a9ebc9fc2a1360cd6ab77cc0f
Sohel Rana <sohelranagtc@gmail.com>
2023-10-08 22:11:50 +0600

    add category

--- src/Pages/Home/Categories/Categories.jsx
+++ src/Pages/Home/Categories/Categories.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Categories = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('AllProducts.json')
+            .then((data) => data.json())
+            .then((data) => {
+                const uniqueCategories = Array.from(
+                    new Set(data.map((product) => product.category))
+                );
+                setProducts(uniqueCategories);
+            });
+    }, []);
+
     return (
         <div>
-            
+            <h1>Unique Categories:</h1>
+            <ul>
+                {products.map((category, index) => (
+                    <li key={index}>{category}
+                        <select className="select select-bordered w-full max-w-xs">
+                            <option disabled selected>Who shot first?</option>
+                            <option>Han Solo</option>
+                            <option>Greedo</option>
+                        </select>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Gallery from './Gallery/Gallery';
+import Categories from './Categories/Categories';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Gallery></Gallery>
+            <Categories></Categories>
         </div>
     );
 };
